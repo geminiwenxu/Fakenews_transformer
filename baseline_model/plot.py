00b525907454c7701f7bbe42500e6c0c25aa0116
Wenxu <geminiwenxu@gmail.com>
2022-11-26 23:59:22 +0100

    increase space btw two subplots

--- baseline_model/plot.py
+++ baseline_model/plot.py
@@ -3,21 +3,19 @@ import torch
 
 
 def plot(history):
-    fig = plt.figure()
-
     plt.subplot(1, 2, 1)
     plt.plot(torch.tensor(history['train_acc']).cpu(), color='r', label='train accuracy')
     plt.plot(torch.tensor(history['val_acc']).cpu(), color='g', label='validation accuracy')
-    plt.title('Training history')
+    plt.title('Accuray')
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
     plt.legend()
     plt.ylim([0, 1.5])
-
+    plt.tight_layout(pad=5.0)
     plt.subplot(1, 2, 2)
     plt.plot(torch.tensor(history['train_loss']).cpu(), color='b', label='train loss')
     plt.plot(torch.tensor(history['val_loss']).cpu(), color='c', label='validation loss')
-    plt.title('Validation history')
+    plt.title('Loss')
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     plt.legend()
