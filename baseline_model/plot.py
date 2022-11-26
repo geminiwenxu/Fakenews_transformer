02f29a052f738a5189b61658990d05346a247d5c
Wenxu <geminiwenxu@gmail.com>
2022-11-26 23:06:49 +0100

    fix a typo

--- baseline_model/plot.py
+++ baseline_model/plot.py
@@ -11,7 +11,7 @@ def plot(history):
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
     plt.legend()
-    plt.ylim([0, 1])
+    plt.ylim([0, 1.5])
 
     plt.subplot(1, 2, 2)
     plt.plot(history['train_loss'], color='b', label='train loss')
@@ -22,26 +22,3 @@ def plot(history):
     plt.legend()
     plt.ylim([0, 1.5])
     plt.savefig("baseline_model.png")
-
-    # plt.show()
-    #
-    #
-    # ax1 = plt.axes()
-    # ax1.plot(history['train_acc'], color='r', label='train accuracy')
-    # ax1.plot(history['val_acc'], color='g', label='validation accuracy')
-    # ax1.set_title('Training history')
-    # ax1.ylabel('Accuracy')
-    # ax1.xlabel('Epoch')
-    # ax1.legend(['train', 'val'], loc='upper left')
-    # ax1.ylim([0, 1.5])
-    # ax1.savefig("baseline_model_accuracy.png")
-    #
-    # ax2 = plt.axes()
-    # ax2.plot(history['train_loss'], color='b', label='train loss')
-    # ax2.plot(history['val_loss'], color='c', label='validation loss')
-    # ax2.set_title('Validation history')
-    # ax2.ylabel('Loss')
-    # ax2.xlabel('Epoch')
-    # ax2.legend()
-    # ax2.ylim([0, 1.5])
-    # ax2.savefig("baseline_model_loss.png")
