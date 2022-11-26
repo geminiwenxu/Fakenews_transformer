1d6a7360a83267c20518c0e491bc42c78fc04a76
Wenxu <geminiwenxu@gmail.com>
2022-11-26 23:30:16 +0100

    convert history to tensor and copy to cpu

--- baseline_model/plot.py
+++ baseline_model/plot.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
+import torch
 
 
 def plot(history):
     fig = plt.figure()
 
     plt.subplot(1, 2, 1)
-    plt.plot(history['train_acc'], color='r', label='train accuracy')
-    plt.plot(history['val_acc'], color='g', label='validation accuracy')
+    plt.plot(torch.tensor(history['train_acc']).cpu(), color='r', label='train accuracy')
+    plt.plot(torch.tensor(history['val_acc']).cpu(), color='g', label='validation accuracy')
     plt.title('Training history')
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
@@ -14,8 +15,8 @@ def plot(history):
     plt.ylim([0, 1.5])
 
     plt.subplot(1, 2, 2)
-    plt.plot(history['train_loss'], color='b', label='train loss')
-    plt.plot(history['val_loss'], color='c', label='validation loss')
+    plt.plot(torch.tensor(history['train_loss']).cpu(), color='b', label='train loss')
+    plt.plot(torch.tensor(history['val_loss']).cpu(), color='c', label='validation loss')
     plt.title('Validation history')
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
