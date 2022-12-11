f404a5c179e41403547c129b874588e2b1eba4b1
Wenxu <geminiwenxu@gmail.com>
2022-12-11 19:25:36 +0100

    copy to gpu

--- fusion_model/fusion_techniques.py
+++ fusion_model/fusion_techniques.py
@@ -6,9 +6,10 @@ import torch.nn.functional as F
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 class FinalNet(nn.Module):
     def __int__(self):
-        super(FinalNet,self).__init__()
+        super(FinalNet, self).__init__()
         self.layer1 = nn.Linear(32, 32)
         self.layer2 = nn.Linear(32, 32)
         self.layer3 = nn.Linear(32, 32)
@@ -21,7 +22,8 @@ class FinalNet(nn.Module):
         output_layer2 = self.layer2(att_layer)
         output_layer3 = self.layer3(att_layer)
         output_layer4 = self.layer4(att_layer)
-        max_layer, inx = torch.max(torch.stack((output_layer1, output_layer2, output_layer3, output_layer4), dim=0), dim=0)
+        max_layer, inx = torch.max(torch.stack((output_layer1, output_layer2, output_layer3, output_layer4), dim=0),
+                                   dim=0)
         final_output_layer = self.final_layer(max_layer)
 
         # add & norm
@@ -42,12 +44,12 @@ def get_final_output(input_att, query):
     :param query: query for add
     :return:final vector
     """
-    layer1 = nn.Linear(32, 32)
-    layer2 = nn.Linear(32, 32)
-    layer3 = nn.Linear(32, 32)
-    layer4 = nn.Linear(32, 32)
-    final_layer = nn.Linear(32, 32)
-    norm1 = torch.nn.LayerNorm(32)
+    layer1 = nn.Linear(32, 32).to(device)
+    layer2 = nn.Linear(32, 32).to(device)
+    layer3 = nn.Linear(32, 32).to(device)
+    layer4 = nn.Linear(32, 32).to(device)
+    final_layer = nn.Linear(32, 32).to(device)
+    norm1 = torch.nn.LayerNorm(32).to(device)
 
     output_layer1 = layer1(input_att)
     output_layer2 = layer2(input_att)
@@ -59,7 +61,7 @@ def get_final_output(input_att, query):
 
     # add & norm
     add_layer = final_output_layer + query
-    final_output = norm1(add_layer)
+    final_output = norm1(add_layer).to(device)
     return final_output
 
 
@@ -78,15 +80,14 @@ def attention_fusion(bert_embedding, feature_embedding):
     head = 1
     attention_layer = 1
 
-
-
     for layer in range(attention_layer):
         for _ in range(head):
             linear1 = nn.Linear(512, 32, bias=True).to(device)
             t_f_K = linear1(feature_embedding)
-            #print(linear1.training)
+
             linear2 = nn.Linear(768, 32, bias=True).to(device)
             t_f_Q = linear2(bert_embedding)
+
             linear3 = nn.Linear(512, 32, bias=True).to(device)
             t_f_V = linear3(feature_embedding)
             t_f_value, t_f_attention = scaled_dot_product(t_f_Q, t_f_K, t_f_V)
@@ -104,18 +105,15 @@ def attention_fusion(bert_embedding, feature_embedding):
 
             # fully connected layers as in paper
 
-            #model = FinalNet()
-            #output_t = model(t_f_value, t_f_Q)
-            #output_f = model(f_t_value, f_t_Q)
-            #ToDo why this is working in function but not in class? Hopefully not wrong
+            # model = FinalNet()
+            # output_t = model(t_f_value, t_f_Q)
+            # output_f = model(f_t_value, f_t_Q)
+            # ToDo why this is working in function but not in class? Hopefully not wrong
             output_t = get_final_output(t_f_value, t_f_Q)
             output_f = get_final_output(f_t_value, f_t_Q)
 
-
-
             # the attention blocks are concat with the inputs for text and features
-            output_cat = torch.cat((bert_embedding,feature_embedding,output_t, output_f), dim=0)
+            output_cat = torch.cat((bert_embedding, feature_embedding, output_t, output_f), dim=0)
 
-
-    #value, inx = torch.max(torch.stack((f_t_value, t_f_value), dim=0), dim=0)
+    # value, inx = torch.max(torch.stack((f_t_value, t_f_value), dim=0), dim=0)
     return output_cat
